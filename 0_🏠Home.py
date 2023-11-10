d7c6f1004de74cd121b41b31f3614f616177c56b
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-11-10 12:50:23 -0300

    home input fixes e rc notebook melhorado

--- 0_🏠Home.py
+++ 0_🏠Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from utils.Global import *
+from math import ceil
 
 
 def titulo_app():
@@ -20,16 +21,23 @@ def rodape():
     
 def formulario():
     with st.form(key='formulario'):
-        nome = st.text_input('Nome').capitalize()
-        idade = st.number_input("Idade:", min_value=0, max_value=120, value=22)
-        sexo = st.radio('Sexo', ('Masculino', 'Feminino'), 0)
-        profissao = st.selectbox('Profissao', ['Não qualificado e não residente', 'Não qualificado e residente', 'Qualificado', 'Altamente qualificado'])
-        moradia = st.selectbox('Moradia', ['Própria', 'Gratuita', 'Alugada'])
-        poupanca = st.selectbox('Poupança', ['Não Declarar', 'Pouco', 'Bastante Rico', 'Rico', 'Moderado'])
-        conta_corrente = st.selectbox('Conta Corrente', ['Pouco', 'Moderado', 'Não Declarar', 'Rico'])
-        valor_do_credito = st.number_input('Valor_do_credito')
-        duracao = st.number_input('Duração em meses', min_value=0)
-        finalidade = st.selectbox('Finalidade', ['Rádio/TV', 'Educação', 'Móveis/Equipamentos', 'Carro', 'Negócio', 'Eletrodomésticos', 'Reparos', 'Férias/Outros'])
+        nome = st.text_input('Nome:').capitalize()
+        idade = st.number_input("Idade:", min_value=0, max_value=120)
+        sexo = st.radio('Sexo:', ('Masculino', 'Feminino'), 0)
+        profissao = st.selectbox('Profissão:', ['Não qualificado e não residente', 'Não qualificado e residente', 'Qualificado', 'Altamente qualificado'])
+        moradia = st.selectbox('Moradia:', ['Própria', 'Gratuita', 'Alugada'])
+        poupanca = st.selectbox('Poupança:', ['Não Declarar', 'Pouco', 'Bastante Rico', 'Rico', 'Moderado'])
+        conta_corrente = st.selectbox('Conta Corrente:', ['Pouco', 'Moderado', 'Não Declarar', 'Rico'])
+        valor_do_credito = st.number_input('Valor do Credito:', min_value=0)
+        duracao = st.number_input('Duração em meses:', min_value=0, max_value=None)
+        finalidade = st.selectbox('Finalidade:', ['Rádio/TV', 'Educação', 'Móveis/Equipamentos', 'Carro', 'Negócio', 'Eletrodomésticos', 'Reparos', 'Férias/Outros'])
+        
+        valor_do_credito = ceil(valor_do_credito)
+        
+        try:
+            duracao = int(duracao)
+        except:
+            st.write('Por favor, digite uma idade válida.')
         
         botao_enviar = st.form_submit_button(label='Enviar')
         
